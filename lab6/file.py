23171bcbdbea4d4feb9f611db9c30c0157a0ff37
SQ005 <shoraev.97@mail.ru>
2023-03-08 16:28:59 +0600

    added

--- lab6/file.py
+++ lab6/file.py
@@ -1,39 +1,39 @@
 #1///
-# import os
-# for root, dirs, files in os.walk('C:\\temp',False):
-#    if len(dirs) > 0: 
-#       print(dirs)
-#    if len(files) > 0: 
-#       print(files)
+import os
+for root, dirs, files in os.walk('C:\\temp',False):
+   if len(dirs) > 0: 
+      print(dirs)
+   if len(files) > 0: 
+      print(files)
         
-#    if len(dirs) > 0 and len(files) > 0: 
-#       joinedlist = dirs + files
-#       print(joinedlist)
+   if len(dirs) > 0 and len(files) > 0: 
+      joinedlist = dirs + files
+      print(joinedlist)
 
 
 #2///
-# import os
-# PATH = 'row.txt'
-# if os.path.isfile(PATH) and os.access(PATH, os.R_OK):
-#    print("File exists and is readable")
-# else:
-#    print("Either the file is missing or not readable")
-# f = open("row.txt", "r")
-# print(f.read())
-# f = open("row.txt", 'a')
-# f.write("Now we can write")
-# f.close()
-# f = open("row.txt", "r")
-# print(f.read())
+import os
+PATH = 'row.txt'
+if os.path.isfile(PATH) and os.access(PATH, os.R_OK):
+   print("File exists and is readable")
+else:
+   print("Either the file is missing or not readable")
+f = open("row.txt", "r")
+print(f.read())
+f = open("row.txt", 'a')
+f.write("Now we can write")
+f.close()
+f = open("row.txt", "r")
+print(f.read())
 
 
 #3///
-# from pathlib import Path
-# my_file = Path("C:\temp")
-# if my_file.is_file():   
-#    print("file exist")
-# else:
-#    print("file not exist")
+from pathlib import Path
+my_file = Path("C:\temp")
+if my_file.is_file():   
+   print("file exist")
+else:
+   print("file not exist")
 
 import os
 print("Test a path exists or not:")
@@ -46,36 +46,37 @@ print(os.path.dirname(path))
 
 
 #4///
-# num_lines = sum(1 for line in open('text.txt'))
-# print(num_lines)
+num_lines = sum(1 for line in open('text.txt'))
+print(num_lines)
 
 
-#5///
-# f = open("text.txt", "a")
-# f.writelines(["See you soon!", "Over and out."])
-# f.close()
-# f = open("text.txt", "r")
-# print(f.read())
+# #5///
+import os
+os.remove("text.txt")
+f = open("text.txt", "x")
+f.writelines(["See you soon!", "Over and out."])
+f.close()
+f = open("text.txt", "r")
+print(f.read())
 
 #6///
-# import string
-# string.ascii_uppercase[:14]
-# for i in string.ascii_uppercase:
-#     print(i)
-
-# f = open("myfile.txt", "x")
+import string, os
+if not os.path.exists("letters"):
+   os.makedirs("letters")
+for letter in string.ascii_uppercase:
+   with open(letter + ".txt", "w") as f:
+       f.writelines(letter)
 
 
 #7///
-
-
-
-
+import shutil
+from shutil import copyfile
+copyfile('row.txt', 'text.txt')
 
 
 #8///
-# import os
-# if os.path.exists("text.txt"):
-#    os.remove("text.txt")
-# else:
-#    print("The file does not exist")
+import os
+if os.path.exists("text.txt"):
+   os.remove("text.txt")
+else:
+   print("The file does not exist")
